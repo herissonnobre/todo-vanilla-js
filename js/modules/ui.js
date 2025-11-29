582a43333ccf6bbc946ab3e5b8e193f4ef1b5467
Herisson Nobre <herisson.carvalho96@gmail.com>
2025-11-29 19:38:02 -0300

    style(css): refine stylesheet to match Material Design 3 theme

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -25,8 +25,26 @@ export function renderTasks(filter = 'all') {
       });
 
       const removeBtn = document.createElement('button');
-      removeBtn.textContent = '✖️';
+
       removeBtn.classList.add('remove-btn');
+      removeBtn.setAttribute('aria-label', 'Remove task');
+
+      // cria o SVG dentro do botão
+      const svgns = "http://www.w3.org/2000/svg";
+      const svg = document.createElementNS(svgns, "svg");
+      svg.setAttribute("height", "1.5rem");
+      svg.setAttribute("width", "1.5rem");
+      svg.setAttribute("viewBox", "0 -960 960 960");
+      svg.setAttribute("fill", "#e3e3e3");
+
+      const path = document.createElementNS(svgns, "path");
+      path.setAttribute("d",
+          "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+      );
+      svg.appendChild(path);
+
+      removeBtn.appendChild(svg);
+
       removeBtn.addEventListener('click', () => {
         removeTask(task.id);
         renderTasks(filter);
